9b18047dbd57bbc72e6fa6aa800ce5dfdfacdcec
jesus1bueno <159319212+jesus1bueno@users.noreply.github.com>
2025-03-08 17:07:34 -0600

    SplashScreen

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-// import SplashScreen from './SplashScreen';
+import SplashScreen from './SplashScreen';
 
 const App = () => {
-  // const [loading, setLoading] = useState(true);
+ const [loading, setLoading] = useState(true);
 
   return (
-    // loading ? (
-    //   <SplashScreen onComplete={() => setLoading(false)} />
-    // ) : (
+     loading ? (
+      <SplashScreen onComplete={() => setLoading(false)} />
+   ) : (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    // )
+     )
   );
 };
 
