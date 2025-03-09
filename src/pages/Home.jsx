06b1bb611fb60c4b72fcb25190e8f8306ff668ef
jesus1bueno <159319212+jesus1bueno@users.noreply.github.com>
2025-03-09 12:53:28 -0600

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
 
   return (
     <div style={styles.homeContainer}>
-      <h2 style={styles.heading}>¡Bienvenido a Tu Espacio de Bienestar!</h2>
+      <img src="src/icons/icon-512x512.png" alt="Logo de la app" />
       <p style={styles.subheading}>Descubre consejos, información y recursos para llevar una vida más saludable.</p>
       <div style={styles.buttonContainer}>
         <button style={styles.btn} onClick={goToLogin}>
@@ -33,27 +33,29 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-    backgroundColor: '#e9ecef', // Color de fondo más suave
-    padding: '20px', // Espaciado alrededor
-    boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)', // Sombra suave
+    justifyContent: 'flex-start', // Alineación hacia arriba
+    minHeight: '100vh', // Cambio de height a minHeight para permitir que se ajuste a la pantalla
+    backgroundColor: '#e9ecef',
+    padding: '20px',
+    boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
+    paddingTop: '10px', // Reducir aún más el espacio superior
   },
   heading: {
-    fontSize: '2.5rem', // Tamaño de fuente más grande
+    fontSize: '2.5rem',
     marginBottom: '10px',
-    color: '#343a40', // Color más oscuro
+    color: '#343a40',
     textAlign: 'center',
   },
   subheading: {
     fontSize: '1.2rem',
-    marginBottom: '30px',
-    color: '#6c757d', // Color gris para el subtítulo
+    marginBottom: '15px', // Reducir el margen inferior para acercar el texto
+    color: '#6c757d',
     textAlign: 'center',
   },
   buttonContainer: {
     display: 'flex',
     gap: '15px',
+    marginTop: '10px', // Reducir el margen superior para los botones
   },
   btn: {
     padding: '12px 25px',
@@ -64,15 +66,14 @@ const styles = {
     borderRadius: '5px',
     cursor: 'pointer',
     transition: 'background-color 0.3s ease, transform 0.2s ease',
-    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.2)', // Sombra en los botones
+    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.2)',
   },
 };
 
-// Efecto hover para los botones
 styles.btnHover = {
   ':hover': {
-    backgroundColor: '#0056b3', // Color más oscuro al pasar el ratón
-    transform: 'scale(1.05)', // Aumentar ligeramente el tamaño
+    backgroundColor: '#0056b3',
+    transform: 'scale(1.05)',
   },
 };
 
