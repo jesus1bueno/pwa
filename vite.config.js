1010c16eef28f3cf8ab387a1115e7cf2f197ab82
jesus1bueno <159319212+jesus1bueno@users.noreply.github.com>
2025-03-20 17:42:31 -0600

    confi

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000, // Usa el puerto asignado por Render
-    host: '0.0.0.0', // Asegura que la aplicación escuche en todas las interfaces de red
-    allowedHosts: ['pwa-7rs5.onrender.com'], // Permite este dominio
+    port: process.env.PORT || 10000,
+    host: '0.0.0.0',
+    strictPort: true,
+  },
+  preview: {
+    port: 10000,
+    host: '0.0.0.0',
   },
 })
+
