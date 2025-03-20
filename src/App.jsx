9f343f5c5a9bee2ba0e6a3a8dc2b0f5ef922f9e3
jesus1bueno <159319212+jesus1bueno@users.noreply.github.com>
2025-03-20 17:37:58 -0600

    como antes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Main from './pages/Main';
@@ -7,28 +7,19 @@ import Register from './pages/Register';
 import SplashScreen from './SplashScreen';
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Registrar el Service Worker cuando la app se monte
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./sw.js', { type: 'module' })
-        .then(registration => console.log('Service Worker registrado:', registration))
-        .catch(error => console.error('Error al registrar el Service Worker:', error));
-    }
-  }, []); // Se ejecuta solo una vez al montar el componente
+ const [loading, setLoading] = useState(true);
 
   return (
-    loading ? (
+     loading ? (
       <SplashScreen onComplete={() => setLoading(false)} />
-    ) : (
+   ) : (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/main" element={<Main />} />
       </Routes>
-    )
+     )
   );
 };
 
