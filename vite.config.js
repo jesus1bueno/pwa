527bc404de59d6bf2c5ab54b86c8434718e9fc07
jesus1bueno <159319212+jesus1bueno@users.noreply.github.com>
2025-03-20 18:26:16 -0600

    htt

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
   server: {
     port: process.env.PORT || 3000, // Usa el puerto asignado por Render
     host: '0.0.0.0', // Asegura que la aplicación escuche en todas las interfaces de red
+    allowedHosts: ['https://pwa-7rs5.onrender.com'], // Permite este dominio
   },
 })
